65002e559c7ec377600f3fa0a84d2458b663d4f0
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-18 11:29:40 +0100

    amount toFixed

--- components/ExpenseOutput/ExpenseItem.jsx
+++ components/ExpenseOutput/ExpenseItem.jsx
@@ -12,7 +12,7 @@ const ExpenseItem = ({ description, amount, date }) => {
                     <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
                 </View>
                 <View style={styles.amountContainer}>
-                    <Text style={styles.amount}>{amount}</Text>
+                    <Text style={styles.amount}>{amount.toFixed(2)}</Text>
                 </View>
             </View>
         </Pressable>
