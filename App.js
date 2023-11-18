392d9d6f524e330724fab1ff431dc9efc4e95829
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-18 15:18:00 +0100

    adding cost button

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
 
 import { GlobalStyles } from './constants/stlyes'
+import IconButton from './components/UI/IconButton';
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -20,7 +21,9 @@ function ExpensesOverview() {
       headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
       headerTintColor: 'white',
       tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-      tabBarActiveTintColor: GlobalStyles.colors.accent500
+      tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      headerRight:({tintColor}) =><IconButton icon='add' size={30} color={tintColor} onPress={() =>{}}/>
+      
     }}>
       <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} options={{
         title: 'Recent Expenses',
