3891f06310d1eccb28a5aa0a61f0160818ebddac
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-18 10:25:12 +0100

    dummy date logic test

--- components/ExpenseOutput/ExpensesOutput.jsx
+++ components/ExpenseOutput/ExpensesOutput.jsx
@@ -3,13 +3,44 @@ import React from 'react'
 import ExpensesSummary from './ExpensesSummary'
 import ExpensesList from './ExpensesList'
 
-const ExpensesOutput = ({expenses}) => {
-  return (
-    <View>
-    <ExpensesSummary/>
-    <ExpensesList />
-    </View>
-  )
+const Dummy_Expenses =[
+    {
+        id:'e1',
+        description:' A pair of shoes',
+        amount: 59.99,
+        date:new Date('2021-11-18')
+    },  {
+        id:'e2',
+        description:' A pair of pants',
+        amount: 89.99,
+        date:new Date('2022-11-18')
+    },
+    {
+        id:'e3',
+        description:' computer',
+        amount: 159.99,
+        date:new Date('2023-11-18')
+    }, 
+     {
+        id:'e4',
+        description:' Telephone',
+        amount: 359.99,
+        date:new Date('2023-10-10')
+    },  {
+        id:'e5',
+        description:' Book',
+        amount: 19.99,
+        date:new Date('2023-09-18')
+    }
+]
+
+const ExpensesOutput = ({ expenses,expensesPeriod }) => {
+    return (
+        <View>
+            <ExpensesSummary periodName={expensesPeriod} expenses={Dummy_Expenses}/>
+            <ExpensesList />
+        </View>
+    )
 }
 
 export default ExpensesOutput
