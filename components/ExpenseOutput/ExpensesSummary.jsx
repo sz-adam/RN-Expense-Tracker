3398db3577cba34483da90ce684404245f059737
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-18 10:38:59 +0100

    add style

--- components/ExpenseOutput/ExpensesSummary.jsx
+++ components/ExpenseOutput/ExpensesSummary.jsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { GlobalStyles } from '../../constants/stlyes'
 
 const ExpensesSummary = ({ periodName, expenses }) => {
 
@@ -7,13 +8,32 @@ const ExpensesSummary = ({ periodName, expenses }) => {
     return sum + expense.amount
   }, 0)
   return (
-    <View>
-      <Text>{periodName}</Text>
-      <Text> ${expenesSum.toFixed(2)}</Text>
-    </View> 
+    <View style={styles.container}>
+      <Text style={styles.period}>{periodName}</Text>
+      <Text style={styles.sum}> ${expenesSum.toFixed(2)}</Text>
+    </View>
   )
 }
 
 export default ExpensesSummary
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  period: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary400,
+
+  },
+  sum: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: GlobalStyles.colors.primary500
+  }
+})
