f532518015aa6d4c2a5295840ec28f6205a49002
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-19 09:43:18 +0100

    statusbar light

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import AllExpenses from './screens/AllExpenses';
 
 import { GlobalStyles } from './constants/stlyes'
 import IconButton from './components/UI/IconButton';
+import ExpensesContextProvider from './store/expenses.context';
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -44,21 +45,22 @@ function ExpensesOverview() {
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
-      <NavigationContainer >
-        <Stack.Navigator screenOptions={{
-          headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-          headerTintColor: 'white'
-        }}>
-          <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} options={{
-            headerShown: false
-          }} />
-          <Stack.Screen name='ManageExpense' component={ManageExpense} options={{
-            presentation: 'modal'
-          }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-
+      <ExpensesContextProvider>
+        <StatusBar style="light" />
+        <NavigationContainer >
+          <Stack.Navigator screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: 'white'
+          }}>
+            <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} options={{
+              headerShown: false
+            }} />
+            <Stack.Screen name='ManageExpense' component={ManageExpense} options={{
+              presentation: 'modal'
+            }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ExpensesContextProvider>
     </>
   );
 }
