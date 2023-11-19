4c8bc15acac14fc81f753744cdbd760bfdcd92a3
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-19 10:42:32 +0100

    styling input

--- components/ManageExpense/Input.jsx
+++ components/ManageExpense/Input.jsx
@@ -1,16 +1,45 @@
 import { StyleSheet, Text, View , TextInput} from 'react-native'
 import React from 'react'
+import { GlobalStyles } from '../../constants/stlyes'
 
 
 const Input = ({ label, textInputConfig }) => {
+
+    const inputStyles =[styles.input]
+
+    if (textInputConfig && textInputConfig.multiline){
+        inputStyles.push(styles.inputMultiline)
+    }
+
     return (
-        <View>
-            <Text>{label}</Text>
-            <TextInput {...textInputConfig} />
+        <View style={styles.inputContainer}>
+            <Text style={styles.label}>{label}</Text>
+            <TextInput style={inputStyles} {...textInputConfig} />
         </View>
     )
 }
 
 export default Input
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    inputContainer:{
+        marginHorizontal: 4,
+        marginVertical: 8,
+    },
+    label:{
+        fontSize:12,
+        color: GlobalStyles.colors.primary100,
+        marginBottom: 4,
+    },
+    input:{
+        backgroundColor: GlobalStyles.colors.primary100,
+        padding:6,
+        borderRadius: 6,
+        fontSize:18,
+        color: GlobalStyles.colors.primary700
+    },
+    inputMultiline:{
+        minHeight:100,
+        textAlignVertical:'top'
+    }
+})
